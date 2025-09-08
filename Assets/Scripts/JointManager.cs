fbc6d594d4682c167fdf48ef1107a87c9bb97119
m.dero <m.dero@student.isartdigital.com>
2025-09-08 16:23:22 +0100

    First spawning script

--- Assets/Scripts/JointManager.cs
+++ Assets/Scripts/JointManager.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class JointManager
+public class JointManager : MonoBehaviour
 {
+
+    [SerializeField] public int _segments = 3;
+
+    [SerializeField] public GameObject joint; // TODO : Add joint at each bone's start and end.
+    [SerializeField] public GameObject bone;
+
+    private Vector3 _initialPosition = new Vector3(0,0,0);
+    void Start()
+    {
+        for (int j = 0; j < _segments; j++)
+        {
+            Instantiate(bone, _initialPosition, Quaternion.identity);
+            _initialPosition = _initialPosition + new Vector3(0, 2, 0); // TODO : Change int raw value.
+        }
+    }
     
 }
